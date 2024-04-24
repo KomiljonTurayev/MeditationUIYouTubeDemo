f18c25cc66a49286c3117f2533c417965c98e034
Komiljon <turayev.komiljon1997@gmail.com>
2024-04-24 17:13:37 +0500

    Initial commit

---
+++ app/src/main/java/com/example/meditationuiyoutubedemo/ui/theme/Shape.kt
@@ -0,0 +1,11 @@
+package com.example.meditationuiyoutubedemo.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.ui.unit.dp
+
+val Shapes = Shapes(
+    small = RoundedCornerShape(4.dp),
+    medium = RoundedCornerShape(4.dp),
+    large = RoundedCornerShape(0.dp)
+)
